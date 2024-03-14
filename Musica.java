6a9d15a0403e4370713c2e9f031ec947c8189d67
lincolnfelixm <94040773+lincolnfelixm@users.noreply.github.com>
2024-03-13 21:24:34 -0300

    Add files via upload

---
+++ Musica.java
@@ -0,0 +1,10 @@
+public class Musica {
+    private String titulo;
+    private String duracao;
+
+    public Musica(String titulo, String duracao) {
+        this.titulo = titulo;
+        this.duracao = duracao;
+    }
+
+}
